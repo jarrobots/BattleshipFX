b7c9f64dcf6276970e2f92e6417f0923c43d3472
Jaroslaw Romanczuk <jaroslaw.romanczuk@st.amu.edu.pl>
2024-05-13 02:25:39 +0200

    Finally, grid editing is working

--- src/main/java/pl/jarrobots/battleshipfx/MainView.java
+++ src/main/java/pl/jarrobots/battleshipfx/MainView.java
@@ -5,6 +5,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -19,6 +20,7 @@ public class MainView extends HBox {
     private final int v2 = 200;
     private Button startButton;
     private Canvas mainGrid;
+    private Canvas actionGrid;
     private Affine affine1;
     private Affine affine2;
     private Canvas viewGrid;
@@ -28,6 +30,7 @@ public class MainView extends HBox {
         this.startButton = new Button("HI");
 
         this.mainGrid = new Canvas(v1,v1);
+        this.actionGrid = new Canvas(v1,v1);
         this.viewGrid = new Canvas(v2, v2);
 
         affine1 = new Affine();
@@ -36,18 +39,26 @@ public class MainView extends HBox {
         affine1.appendScale(v1/10f,v1/10f);
         affine2.appendScale(v2/10f,v2/10f);
 
-        draw(mainGrid,Color.LIGHTBLUE, affine1);
-        draw(viewGrid, Color.WHITESMOKE,  affine2);
+        GraphicsContext gc3 = mainGrid.getGraphicsContext2D();
+        GraphicsContext gc2 = viewGrid.getGraphicsContext2D();
+        GraphicsContext gc1 = actionGrid.getGraphicsContext2D();
 
-        mainGrid.setOnMousePressed(this::drawOnClick);
 
+        draw(gc3,Color.LIGHTBLUE, affine1);
+        draw(gc2, Color.WHITESMOKE, affine2);
+
+        actionGrid.setOnMousePressed( e -> { this.drawOnClick(e, gc1);});
+
+        Pane grids = new Pane();
+        grids.getChildren().addAll(mainGrid,actionGrid);
+        actionGrid.toFront();
         VBox vBox = new VBox();
         vBox.getChildren().addAll(this.viewGrid, this.startButton);
-        this.getChildren().addAll(this.mainGrid, vBox);
+        this.getChildren().addAll(grids, vBox);
     }
 
-    private void draw(Canvas canvas, Color c, Affine affine){
-        GraphicsContext context = canvas.getGraphicsContext2D();
+    private void draw(GraphicsContext context, Color c, Affine affine){
+
         context.setTransform(affine);
         context.setFill(c);
         context.fillRect(0,0,v1,v1);
@@ -62,16 +73,16 @@ public class MainView extends HBox {
         for(int j = 0; j<=10;j++){
             context.strokeLine(0,j,10,j);
         }
-        context.setFill(Color.RED);
-        context.fillOval(30.0,30.0,60.0,60.0);
     }
-    private void drawOnClick(MouseEvent event){
+    private void drawOnClick(MouseEvent event, GraphicsContext context){
 
         try {
             Point2D coords = this.affine1.inverseTransform(event.getX(), event.getY());
             int simX = (int) coords.getX();
             int simY = (int) coords.getY();
-            System.out.println(simX + " , "+simY);
+            context.setFill(Color.RED);
+            context.fillRect(simX*v1/10f,simY*v1/10f, v1/10f, v1/10f);
+            System.out.println(simX + " " +simY);
         }catch (NonInvertibleTransformException e){
             e.printStackTrace();
         }
